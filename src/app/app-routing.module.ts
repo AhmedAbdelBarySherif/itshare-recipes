21bb66faec36642394cfee366880ca04d7582200
ahmed-sherif <ahmedabdelbarysherif@gmail.com>
2019-01-07 14:34:45 +0200

    Done auth gaurd service and add rest of components and add canActivate in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,8 @@
+import { AuthGaurdService } from './services/auth-gaurd.service';
+import { AdminManageUsersComponent } from './components/admin-manage-users/admin-manage-users.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { HomeComponent } from './components/home/home.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,12 +13,16 @@ import { ProductsComponent } from './components/products/products.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'products', component: ProductsComponent },
-  { path: 'orders', component: MyOrdersComponent },
-  { path: 'admin/orders', component: AdminOrdersComponent },
-  { path: 'admin/products', component: ProductsComponent },
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductsComponent, canActivate: [AuthGaurdService] },
   { path: 'shopping-cart', component: ShoppingCartComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGaurdService] },
+  { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGaurdService] },
+  { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGaurdService] },
+  { path: 'admin/products', component: ProductsComponent, canActivate: [AuthGaurdService] },
+  { path: 'admin/users', component: AdminManageUsersComponent, canActivate: [AuthGaurdService] },
+  { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGaurdService] },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
+// Main ng modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
+// Services and httpclient
 import { HttpClientModule } from '@angular/common/http';
 import { TryService } from './services/try.service';
 import { AuthService } from './services/auth.service';
+import { AuthGaurdService } from './services/auth-gaurd.service';
 
+// Routing file
 import { AppRoutingModule } from './app-routing.module';
 
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -17,13 +22,20 @@ import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { HomeComponent } from './components/home/home.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { MyordersComponent } from './components/myorders/myorders.component';
+import { AdminManageUsersComponent } from './components/admin-manage-users/admin-manage-users.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+// Firebase modules
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuth } from '@angular/fire/auth';
 
+// Environment file contains firebase config
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -34,7 +46,12 @@ import { environment } from '../environments/environment';
     MyOrdersComponent,
     AdminOrdersComponent,
     ShoppingCartComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyordersComponent,
+    AdminManageUsersComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +68,8 @@ import { environment } from '../environments/environment';
   providers: [
     TryService,
     AngularFireAuth,
-    AuthService
+    AuthService,
+    AuthGaurdService
   ],
   bootstrap: [AppComponent]
 })
